a52ea11051a676a975d1214ff9348e1336b61ed7
Wagner R. de Morais <wagnerrdemorais@gmail.com>
2020-10-07 22:18:05 -0300

    segunda aula

--- lib/ui/HomePage.dart
+++ lib/ui/HomePage.dart
@@ -1,7 +1,10 @@
 import 'dart:convert';
 
+import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -20,7 +23,7 @@ class _HomePageState extends State<HomePage> {
           "https://api.giphy.com/v1/gifs/trending?api_key=GUIvrwuEoxD9RYWkL8sic5ApNlZ0D2k5&limit=20&rating=g");
     } else {
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=GUIvrwuEoxD9RYWkL8sic5ApNlZ0D2k5&q$_search=&limit=20&offset=$offset&rating=g&lang=en");
+          "https://api.giphy.com/v1/gifs/search?api_key=GUIvrwuEoxD9RYWkL8sic5ApNlZ0D2k5&q=$_search&limit=19&offset=$offset&rating=g&lang=en");
     }
     return json.decode(response.body);
   }
@@ -53,17 +56,99 @@ class _HomePageState extends State<HomePage> {
                 border: OutlineInputBorder(),
               ),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  offset = 0;
+                });
+              },
             ),
           ),
           Expanded(
               child: FutureBuilder(
             future: _getGifs(),
             builder: (context, snapshot) {
-               
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                case ConnectionState.none:
+                  return Container(
+                    width: 200,
+                    height: 200,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      strokeWidth: 5.0,
+                    ),
+                  );
+                default:
+                  if (snapshot.hasError)
+                    return Container();
+                  else
+                    return _createGifTable(context, snapshot);
+              }
             },
           )),
         ],
       ),
     );
+  }
+
+  int _getCount(List data) {
+    if (_search == null || _search.isEmpty) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: _getCount(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if (_search == null || index < snapshot.data["data"].length) {
+            return GestureDetector(
+              child: FadeInImage.memoryNetwork(
+                  fit: BoxFit.cover,
+                  height: 300.0,
+                  placeholder: kTransparentImage,
+                  image: snapshot.data["data"][index]["images"]["fixed_height"]
+                      ["url"]),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            GifPage(snapshot.data["data"][index])));
+              },
+              onLongPress: () {
+                Share.share(snapshot.data["data"][index]["images"]
+                    ["fixed_height"]["url"]);
+              },
+            );
+          } else {
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add, color: Colors.white, size: 70),
+                    Text(
+                      "Carregar mais ...",
+                      style: TextStyle(color: Colors.white, fontSize: 22.0),
+                    )
+                  ],
+                ),
+                onTap: () {
+                  setState(() {
+                    offset += 19;
+                  });
+                },
+              ),
+            );
+          }
+        });
   }
 }
